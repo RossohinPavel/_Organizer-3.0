a94060223c2ab146af0dc4d5ef07bd35fd3651bd
RossohinPavel <rossohinpavel@yandex.ru>
2023-07-21 10:09:28 +0300

    Образец паралельного запуска циклов программы

---
+++ test.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+import threading as th
+import time
+
+
+start = True
+
+def func(func):
+    global start
+    value = 0
+    while start:
+        print(f'I\'am second thread. Value = {value}')
+        time.sleep(2)
+        value += 1
+        func(text=value)
+
+
+root = tk.Tk()
+text = tk.Label(master=root, text='Some text')
+text.place(x=1, y=1)
+
+if __name__ == '__main__':
+    thr = th.Thread(target=func, args=(text.config, ))
+    thr.start()
+    root.mainloop()
+    start = False
