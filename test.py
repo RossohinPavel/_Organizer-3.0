3761dfe4f7ffa38a9e5eae779d7fd7f1ecc3c95d
RossohinPavel <rossohinpavel@yandex.ru>
2023-08-31 15:34:46 +0300

    Начата работа над планировщиком

--- test.py
+++ test.py
@@ -1,25 +1,84 @@
-import tkinter as tk
-import threading as th
+import threading
 import time
 
 
-def f_sleep():
-    for i in range(5, 0, -1):
-        print(f"wait {i}")
-        time.sleep(1)
-    print('weak up')
+class Task:
+    def __init__(self, func, args=None, kwargs=None):
+        self.func = func
+        self.args = args if args else ()
+        self.kwargs = kwargs if kwargs else {}
+        self.status = 'created'
+        self._thread = None
+
+    def run(self):
+        if self._thread is None:
+            self.status = 'in_progress'
+            self._thread = threading.Thread(target=self.func, args=self.args, kwargs=self.kwargs)
+            self._thread.start()
+        self.after_run()
+
+    def after_run(self): pass
+
+    def __bool__(self):
+        return self.status != 'finished'
 
 
-def main():
-    th1 = th.Thread(target=f_sleep)
-    th1.start()
+class ParallelTask(Task):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__delay = 5
+        self.__timer = self.__delay
+
+    def after_run(self):
+        if self.__timer > 0:
+            self.__timer -= 1
+        elif self.__timer <= 0 and not self._thread.is_alive():
+            self._thread = None
+            self.__timer = self.__delay
 
 
-root = tk.Tk()
-root.geometry('100x100')
+class ConsistentTask(Task):
+    def after_run(self):
+        if not self._thread.is_alive():
+            self.status = 'finished'
 
-txt = tk.Button(master=root, text='text', command=main)
-txt.pack(anchor='center', expand=1)
+
+class IPlanner:
+    __alive = False
+
+    def __init__(self):
+        self._queue = []
+        self.__thread = threading.Thread(target=self.__run_main_thread)
+
+    def __run_main_thread(self):
+        while self.__alive:
+            for task in self._queue:
+                task.run()
+                if not task:
+                    self._queue.remove(task)
+            time.sleep(1)
+
+    def reg(self, func, args=None, kwargs=None, parallels=False):
+        if parallels:
+            self._queue.append(ParallelTask(func, args, kwargs))
+        else:
+            self._queue.append(ConsistentTask(func, args, kwargs))
+        if not self.__alive:
+            self.__alive = True
+            self.__thread.start()
+
+    def kill(self):
+        self.__alive = False
 
 
-root.mainloop()
+planer = IPlanner()
+
+planer.reg(print, ('Я принтуюсь каждые 5 секунд', ), parallels=True)
+planer.reg(print, ('Я должен принтануться всего 1 раз', ))
+time.sleep(10)
+planer.reg(print, ('Я должен принтануться всего 1 раз', ))
+print(planer._queue)
+time.sleep(5)
+print(planer._queue)
+time.sleep(6)
+planer.kill()
