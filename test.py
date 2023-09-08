efc27d5300c3c26ceb2fba52c41a109849976891
Work Station <rossohinpavel@yandex.ru>
2023-09-08 17:43:20 +0300

    в очередной раз думаем над либой

--- test.py
+++ test.py
@@ -1,12 +1,31 @@
-class Test1:
-    atr = None
+class Property:
+    attr = 'property'
+    
+    def __new__(cls, category, default_values):
+        return cls.attr, cls.add_defaults(category)
+
+    @staticmethod
+    def add_defaults(category):
+        return ''
 
 
-class Test2(Test1):
-    def __init__(self):
-        Test1.atr = self
+class Product:
+    propertys = (Property, )
+    def __new__(cls, default_values):
+        return dict(prop(cls, default_values) for prop in cls.propertys)
+
+class Photobook(Product):
+    pass
+        
+
+class ProductGenerator:
+    __prd_dct = {'Photobook': Photobook}
+    
+    classmethod
+    def create(cls, category, default_values=False):
+        return cls.__prd_dct[category](default_values)
 
 
-test = Test2()
+test = ProductGenerator()
 
-print(test.atr.atr)
+print(test.create('Photobook', True))
