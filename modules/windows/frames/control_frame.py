3b4c1cb59c97ff1e77d956154565d1773e9960f7
RossohinPavel <rossohinpavel@yandex.ru>
2023-10-10 18:56:07 +0300

    подготовка к написанию обработчика обложек

--- modules/windows/frames/control_frame.py
+++ modules/windows/frames/control_frame.py
@@ -1,14 +1,16 @@
 from .common import *
-from modules.windows.settings import SettingsWindow
-from modules.windows.library import LibraryWindow
+from modules.windows.settings import SettingsWindow as StgWin
+from modules.windows.library import LibraryWindow as LibWin
 
 
 class ControlFrame(LabeledFrame):
     """Фрейм для отрисовки кнопок управления приложением"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, text='Управление приложением', **kwargs)
-        self.container.pack(expand=1)
         root_master = self.master.master.master
-        src.MyButton(master=self.container, text="Настройки", command=lambda: SettingsWindow(root_master), width=16).pack(pady=(55, 4))
-        src.MyButton(master=self.container, text="Библиотека", command=lambda: LibraryWindow(root_master), width=16).pack()
-        src.MyButton(master=self.container, text="Информация", command=None, width=16).pack(pady=4)
+        frame = src.ttk.Frame(master=self.container)
+        frame.pack(anchor='center', expand=1)
+        src.MyButton(master=frame, text="Клиенты", command=lambda: LibWin(root_master), width=16).pack(pady=(0, 3))
+        src.MyButton(master=frame, text="Библиотека", command=lambda: LibWin(root_master), width=16).pack(pady=(0, 3))
+        src.MyButton(master=frame, text="Информация", command=None, width=16).pack(pady=(0, 3))
+        src.MyButton(master=frame, text="Настройки", command=lambda: StgWin(root_master), width=16).pack()
