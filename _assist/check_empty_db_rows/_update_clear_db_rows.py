b708729261d3eca80c15fae3802943fc89638cab
Work Station <rossohinpavel@yandex.ru>
2023-10-14 16:55:22 +0300

    скан 14

--- _assist/check_empty_db_rows/_update_clear_db_rows.py
+++ _assist/check_empty_db_rows/_update_clear_db_rows.py
@@ -1,5 +1,6 @@
 import pickle
 import sqlite3
+from os.path import exists as p_exists
 
 
 MEM = {}
@@ -7,11 +8,12 @@ MEM = {}
 
 def get_solutions(line):
     if line[1] not in MEM:
+        print('-'*100)
         print(line)
-        sol = input('Вы хотите обновить данные для этого заказа (y/n/q)?')
-        if sol == 'y':
+        sol = input('Вы хотите обновить данные для этого заказа (д/н/в)?')
+        if sol == 'д':
             return sol
-        if sol == 'n':
+        if sol == 'н':
             MEM[line[1]] = sol
             with open('solutions.mem', 'wb') as file:
                 pickle.dump(MEM, file)
@@ -30,15 +32,19 @@ def get_new_data(line):
     return *line[:3], customer_name, customer_address, price
 
 
-with open('solutions.mem', 'rb') as file:
-    MEM.update(pickle.load(file))
+if p_exists('solutions.mem'):
+    with open('solutions.mem', 'rb') as file:
+        MEM.update(pickle.load(file))
+else:
+    with open('solutions.mem', 'wb') as file:
+        pickle.dump(MEM, file)
 
 
 with sqlite3.connect('../../data/log.db') as connect:
     cursor = connect.cursor()
     cursor.execute(f'SELECT * FROM Orders WHERE customer_name="Unknown" OR customer_name="Диспетчер ФотокнигиОптом" OR price=0.0')
-    for line in cursor.fetchall():
-        if get_solutions(line) == 'y':
+    for line in reversed(cursor.fetchall()):
+        if get_solutions(line) == 'д':
             new_line = get_new_data(line)
             cursor.execute(f'UPDATE Orders SET customer_name=\'{new_line[-3]}\', customer_address=\'{new_line[-2]}\', price={new_line[-1]} WHERE name=\'{line[1]}\'')
             connect.commit()
