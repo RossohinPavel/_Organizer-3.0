8b3a1e9b23dba6e7e71948f4f5b5eb4b3d5f5e2a
RossohinPavel <rossohinpavel@yandex.ru>
2023-10-18 08:40:55 +0300

    подготовка к оптимизации библиотеки и перевода на __slots__

--- modules/_safe_connect.py
+++ modules/_safe_connect.py
@@ -4,26 +4,23 @@ from threading import Lock
 
 class SafeConnect:
     """Менеджер контекста для безопасного подключения к базам данных sqlite3"""
-    __slots__ = '__db_name', 'connect', 'cursor'
+    __slots__ = '__db_name', '__lock', 'connect', 'cursor'
 
-    def __new__(cls, db_name: str):
-        if not getattr(cls, f'__{db_name}l', None):
-            setattr(cls, f'__{db_name}l', Lock())
-        obj = super().__new__(cls)
-        obj.__db_name = db_name
-        obj.connect = obj.cursor = None
-        return obj
+    def __init__(self, db_name: str):
+        self.__db_name = db_name
+        self.__lock = Lock()
+        self.connect = self.cursor = None
 
     def __enter__(self):
         """При входе в менеджер создаем подключение к бд и получаем ее курсор. Блокируем сторонний доступ"""
-        getattr(self, f'__{self.__db_name}l').acquire()
+        self.__lock.acquire()
         self.connect = sqlite3.connect(f'data/{self.__db_name}')
         self.cursor = self.connect.cursor()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         """При выходе из менеджера, закрываем подключение и снимаем блок с доступа."""
-        getattr(self, f'__{self.__db_name}l').release()
+        self.__lock.release()
         self.cursor = None
         self.connect.close()
         return False
