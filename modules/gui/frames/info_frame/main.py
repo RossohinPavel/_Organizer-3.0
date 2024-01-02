6bbbc71aa6133851193e340b3b7d4dd06837969e
Home <rossohinpavel@yandex.ru>
2024-01-02 13:36:05 +0300

    небольшие фиксы

--- modules/gui/frames/info_frame/main.py
+++ modules/gui/frames/info_frame/main.py
@@ -21,12 +21,12 @@ class InfoFrame(ttk.Frame):
         """Вспомогательня ф-я для отрисовки фрема с черточкой в начале"""
         frame = ttk.Frame(master, padding=(15, 0, 0, 0))
         frame.pack(anchor=ttkc.W)
-        ttk.Separator(frame, orient='horizontal').place(width=15, rely=0.44, x=-15)
+        ttk.Separator(frame, orient='horizontal').place(width=12, rely=0.44, x=-12)
         return frame
     
     def draw_tracker_frame(self, master: Any) -> None:
         """Отрисовка виджетов управления Трекером заказов"""
-        HeaderLabel(master, text='Трекер заказов', anchor='n').pack(fill=ttkc.X, pady=(0, 2))
+        HeaderLabel(master, text='Трекер заказов', anchor='n').pack(fill=ttkc.X, pady=(0, 3))
         self.draw_status_line(master)
         self.draw_tracker_mode_line(master)
         self.draw_log_depth_line(master)
@@ -40,10 +40,10 @@ class InfoFrame(ttk.Frame):
     
     def draw_status_line(self, master: Any) -> None:
         """Группа фреймов отображения статуса трекера"""
-        ttk.Label(master, text='Статус:', style='minipadding.TLabel').pack(anchor=ttkc.W)
+        ttk.Label(master, text='Статус:', style='minipadding.TLabel').pack(anchor=ttkc.W, pady=(0, 3))
         frame = self._draw_separator_frame(master)
         status_var = ttk.StringVar(frame, value='Повтор в 11:25')
-        ttk.Label(frame, text='Повтор в 11:25', style='minipadding.TLabel').pack(pady=(2, 0))
+        ttk.Label(frame, text='Повтор в 11:25', style='minipadding.TLabel').pack()
     
     def draw_tracker_mode_line(self, master: Any) -> None:
         """Отрисовка группы фреймов управления режимом трекера"""
